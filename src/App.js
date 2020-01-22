e8b6d89d1685dc83504b4f4c28a20661231f95a6
Matilda Arvidsson <matilda.j.arvidsson@gmail.com>
2020-01-22 19:27:05 +0100

    Imported component and fixed toggle issue

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { TopMenu } from 'lib/TopMenu'
+import { ChipsCard } from "lib/FilterChips/ChipsCard"
+
 
 export const App = () => {
   return (
     <div>
-      <TopMenu 
-      title="Techquise Components Library"
+      <TopMenu
+        title="Techquise Components Library"
       />
+
+      <ChipsCard />
     </div>
   )
 }
